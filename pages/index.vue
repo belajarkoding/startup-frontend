510458f2a80a2781ae3af002c103d26f4473d83e
Galih Pratama <hanamura.iost@gmail.com>
2020-08-31 11:52:09 +0700

    Header

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,112 @@
 <template>
-  <div class="container mx-auto mt-5">
-    <div>
-      <h1 class="title text-lg">Homepage</h1>
-      <ul class="list-disc">
-        <li>
-          <nuxt-link to="/login">Login</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/register">Register</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/register-success">Register (Success)</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/upload">Upload</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/upload-filled">Upload (Filled)</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/fund-success">Fund (Success)</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/404">404 Page</nuxt-link>
-        </li>
-      </ul>
-    </div>
+  <div class="landing-page">
+    <section class="landing-hero pt-5">
+      <div class="header__bg"></div>
+      <div class="container mx-auto relative">
+        <header class="flex items-center">
+          <div style="height: 54px;" class="pr-5">
+            <img src="/logo.svg" alt="logo" class="h-full" />
+          </div>
+          <ul class="flex items-center">
+            <li>
+              <a
+                class="text-white hover:text-teal-500 text-lg px-4 py-3"
+                href="/"
+                >Home</a
+              >
+            </li>
+            <li>
+              <a
+                class="text-white hover:text-teal-500 text-lg px-4 py-3"
+                href="/pricing"
+                >Project</a
+              >
+            </li>
+            <li>
+              <a
+                class="text-white hover:text-teal-500 text-lg px-4 py-3"
+                href="/features"
+                >Features</a
+              >
+            </li>
+            <li>
+              <a
+                class="text-white hover:text-teal-500 text-lg px-4 py-3"
+                href="/story"
+                >Success Stories</a
+              >
+            </li>
+          </ul>
+          <ul class="flex ml-auto items-center mt-2">
+            <li>
+              <nuxt-link
+                to="/register"
+                class="inline-block bg-transparent border-white border hover:bg-white hover:bg-opacity-25 text-white font-light w-40 text-center px-6 py-1 text-lg rounded-full mr-4"
+              >
+                Sign Up
+              </nuxt-link>
+            </li>
+            <li>
+              <nuxt-link
+                to="/login"
+                class="inline-block bg-transparent border-white border hover:bg-white hover:bg-opacity-25 text-white font-light w-40 text-center px-6 py-1 text-lg rounded-full"
+              >
+                My Account
+              </nuxt-link>
+            </li>
+          </ul>
+        </header>
+        <div class="flex items-center pt-10 px-5 md:px-0">
+          <div class="w-1/2">
+            <h1 class="text-4xl text-white mb-5">
+              We helps <u class="hero-underline">startup</u> to <br />
+              getting started & <u class="hero-underline">funding</u> <br />
+              their truly needs
+            </h1>
+            <p class="text-white text-xl font-light mb-8">
+              Fund the best idea to become <br />
+              a real product and be the contributor
+            </p>
+            <button
+              @click="$router.push({ path: '/' })"
+              class="block bg-orange-button hover:bg-green-button text-white font-semibold px-12 py-3 text-xl rounded-full"
+            >
+              Find a Project
+            </button>
+          </div>
+          <div class="w-1/2 flex justify-center">
+            <img src="/hero-image@2x.png" alt="crowdfunding project" />
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style lang="scss">
+.landing-hero {
+}
+.header__bg {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  width: 100%;
+  height: 640px;
+  background-image: url('/auth-background.svg');
+  background-position: top right;
+  background-repeat: no-repeat;
+  background-color: #3b41e3;
+  transform: skewY(-6deg);
+  transform-origin: top left;
+}
+
+.hero-underline {
+  text-decoration-color: #1abc9c;
+}
+</style>
