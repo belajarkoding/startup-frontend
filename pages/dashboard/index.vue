320286b72deba153689937f4c8a7924822bcc0b5
Galih Pratama <hanamura.iost@gmail.com>
2020-10-29 18:50:10 +0700

    Dashboard project, dashboard transaction

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -34,27 +34,37 @@
       </div>
       <hr />
       <div class="block mb-2">
-        <div class="w-full lg:max-w-full lg:flex mb-4" v-for="i in 5" :key="i">
+        <div
+          class="w-full lg:max-w-full lg:flex mb-4"
+          v-for="campaign in campaigns.data"
+          :key="campaign.id"
+        >
           <div
-            class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-            style="background-color: #bbb"
+            class="h-48 lg:h-auto lg:w-64 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+            :style="
+              'background-color: #bbb; background-position: center; background-image: url(' +
+              $axios.defaults.baseURL +
+              '/' +
+              campaign.image_url +
+              ')'
+            "
           ></div>
           <nuxt-link
-            :to="'/dashboard/projects/' + i"
-            class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-8 flex flex-col justify-between leading-normal"
+            :to="'/dashboard/projects/' + campaign.id"
+            class="w-full border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-8 flex flex-col justify-between leading-normal"
           >
             <div class="mb-8">
               <div class="text-gray-900 font-bold text-xl mb-1">
-                Cari Uang Buat Gunpla
+                {{ campaign.name }}
               </div>
               <p class="text-sm text-gray-600 flex items-center mb-2">
-                Rp. 200.000.000 &middot; 80%
+                Rp.
+                {{ new Intl.NumberFormat().format(campaign.goal_amount) }}
+                &middot;
+                {{ (campaign.current_amount / campaign.goal_amount) * 100 }}%
               </p>
               <p class="text-gray-700 text-base">
-                With N-key rollover (NKRO on wired mode only) you can register
-                as many keys as you can press at once without missing out
-                characters. It allows to use all the same media keys as
-                conventional macOS.
+                {{ campaign.short_description }}
               </p>
             </div>
             <div class="flex items-center">
@@ -75,6 +85,12 @@
 <script>
 export default {
   middleware: 'auth',
+  async asyncData({ $axios, app }) {
+    const campaigns = await $axios.$get(
+      '/api/v1/campaigns?user_id=' + app.$auth.user.id
+    )
+    return { campaigns }
+  },
 }
 </script>
 
--- pages/dashboard/transactions.vue
+++ pages/dashboard/transactions.vue
@@ -28,20 +28,33 @@
       </div>
       <hr />
       <div class="block mb-2">
-        <div class="w-full lg:max-w-full lg:flex mb-4" v-for="i in 5" :key="i">
+        <div
+          class="w-full lg:max-w-full lg:flex mb-4"
+          v-for="transaction in transactions.data"
+          :key="transaction.id"
+        >
           <div
-            class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-            style="background-color: #bbb"
+            class="h-48 lg:h-auto lg:w-64 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+            :style="
+              'background-color: #bbb; background-position: center; background-image: url(' +
+              $axios.defaults.baseURL +
+              '/' +
+              transaction.campaign.image_url +
+              ')'
+            "
           ></div>
           <div
             class="w-full border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-8 flex flex-col justify-between leading-normal"
           >
             <div>
               <div class="text-gray-900 font-bold text-xl mb-1">
-                Cari Uang Buat Gunpla
+                {{ transaction.campaign.name }}
               </div>
               <p class="text-sm text-gray-600 flex items-center mb-2">
-                Rp. 200.000.000 &middot; 12 September 2020
+                Rp.
+                {{ new Intl.NumberFormat().format(transaction.amount) }}
+                &middot; {{ transaction.created_at }} &middot;
+                {{ transaction.status }}
               </p>
             </div>
           </div>
@@ -57,6 +70,10 @@
 <script>
 export default {
   middleware: 'auth',
+  async asyncData({ $axios }) {
+    const transactions = await $axios.$get('/api/v1/transactions')
+    return { transactions }
+  },
 }
 </script>
 
